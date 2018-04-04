d9978d7b96f7d291e967b850018520e45c870c23
Jorge Macias <jorgemacias@hotmail.com>
2018-04-04 10:12:15 -0500

    Neza

--- enc28j60MQTT_v2.ino
+++ enc28j60MQTT_v2.ino
@@ -1,99 +1,94 @@
+// GOOD ONE
 #include <UIPEthernet.h>
 #include "PubSubClient.h"
-#include "DHT.h"
-//kennelijk  geeft update van DHT sensor library boven 1.2.1 een fout
-#define CLIENT_ID       "Hal"
-//#define TOPIC           "temp"
+#define CLIENT_ID   "Hal"
+//#define SERVER      "mercury.dwim.mx"
+//#define USERNAME    "dwim"
+//#define PASSWORD    "gpscontrol1"
+#define SERVER      "c4neza.dwim.space"
+#define USERNAME    "c4neza"
+#define PASSWORD    "Neza1234"
 #define PUBLISH_DELAY   3000
-#define DHTPIN          3
-#define DHTTYPE         DHT11
-bool statusKD = HIGH;
-bool statusBD = HIGH;
-bool statusGD = HIGH;
-bool pir = LOW;
-int lichtstatus;
+#define OPTO_COUPLER_PIN 4
+#define LED_PIN 5
+#define BUTTON_PIN1 16
+
 uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
 //IPAddress mqttServer(192,168,1,4);
-
+//For  Local 
 EthernetClient ethClient;
 PubSubClient mqttClient;
-DHT dht(DHTPIN, DHTTYPE);
 
-long previousMillis;
-//pinMode(4,INPUT_PULLUP);
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println("");
+  if (length>0){
+      if (payload[0]==48)
+      {
+        //do stuff here because it was a match
+        Serial.println("off");
+        digitalWrite(OPTO_COUPLER_PIN,LOW);
+      }         
+      if(payload[0]==49){
+        Serial.println("on");
+        digitalWrite(OPTO_COUPLER_PIN,HIGH);
+      }
+      if(payload[0]==82){
+        Serial.println("Ring");
+        digitalWrite(OPTO_COUPLER_PIN,HIGH);
+        delay(2000);
+        digitalWrite(OPTO_COUPLER_PIN,LOW);
+      }
+  }
+  Serial.println();
+}
 
 void setup() {
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
+  pinMode(OPTO_COUPLER_PIN,OUTPUT);
+  pinMode(LED_PIN,OUTPUT);
+  pinMode(BUTTON_PIN1,INPUT);
+  digitalWrite(LED_PIN,LOW);
+
+
+
   // setup serial communication
-  /*
-    Serial.begin(9600);
-    while(!Serial) {};
-    Serial.println(F("MQTT Arduino Demo"));
-    Serial.println();
-  */
+  Serial.begin(115200);
+  while(!Serial) {};
+  Serial.println();
+  Serial.println(F("MQTT Arduino Demo"));
+  Serial.println();
+  Serial.println(digitalRead(BUTTON_PIN1));
   // setup ethernet communication using DHCP
   if (Ethernet.begin(mac) == 0) {
     //Serial.println(F("Unable to configure Ethernet using DHCP"));
     for (;;);
   }
-  /*
-    Serial.println(F("Ethernet configured via DHCP"));
-    Serial.print("IP address: ");
-    Serial.println(Ethernet.localIP());
-    Serial.println();
-  */
+  Serial.println(F("Ethernet configured via DHCP"));
+  Serial.print("IP address: ");
+  Serial.println(Ethernet.localIP());
+  Serial.println();
   // setup mqtt client
   mqttClient.setClient(ethClient);
-  //mqttClient.setServer(mqttServer, 1883);
-  mqttClient.setServer("test.mosquitto.org", 1883);
-  //Serial.println(F("MQTT client configured"));
-
-  // setup DHT sensor
-  dht.begin();
-  //Serial.println(F("DHT sensor initialized"));
-
-  //Serial.println();
-  // Serial.println(F("Ready to send data"));
-  previousMillis = millis();
+  mqttClient.setServer(SERVER, 1883);
+  mqttClient.setCallback(callback);
+  Serial.println(F("MQTT client configured"));
 }
 
 void loop() {
-  statusBD = digitalRead(4);
-  statusGD = digitalRead(5);
-  statusKD = (digitalRead(6));
-  lichtstatus = analogRead(A0);
-  pir = digitalRead(7);
 
-  // it's time to send new data?
-  if (millis() - previousMillis > PUBLISH_DELAY) {
-    sendData();
-    previousMillis = millis();
+   
+  //Serial.println(); 
+ if (!mqttClient.connected()) {
+   if (mqttClient.connect(CLIENT_ID,USERNAME,PASSWORD)) {
+    mqttClient.subscribe("alarm");
+    Serial.println("Subscribed to alarm");
+    digitalWrite(LED_PIN,HIGH);
   }
-
+ }
   mqttClient.loop();
 }
-
-void sendData() {
-
-  char msgBuffer[20];
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  //  Serial.print("Temperature: ");
-  // Serial.println(t);
-
-  if (mqttClient.connect(CLIENT_ID)) {
-    mqttClient.publish("hal/temp", dtostrf(t, 6, 2, msgBuffer));
-    mqttClient.publish("hal/humid", dtostrf(h, 6, 2, msgBuffer));
-    mqttClient.publish("hal/deur", (statusBD == HIGH) ? "OPEN" : "DICHT");
-    mqttClient.publish("hal/garage", (statusGD == HIGH) ? "OPEN" : "DICHT");
-    mqttClient.publish("hal/bel", (statusKD == HIGH) ? "OPEN" : "DICHT");
-    mqttClient.publish("hal/l", dtostrf(lichtstatus, 4, 0, msgBuffer));
-    mqttClient.publish("hal/p", (pir== HIGH) ? "OPEN" : "DICHT");
-    //  mqttClient.publish(TOPIC, dtostrf(t, 6, 2, msgBuffer));
-
-  }
-}
-
-
